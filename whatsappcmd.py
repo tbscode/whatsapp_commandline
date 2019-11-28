7b826f3fabce34ad9463331226b6687a596c33ee
Tim S <tim@timschupp.de>
2019-11-28 02:10:59 +0100

    added message viewing and contact listing

--- whatsappcmd.py
+++ whatsappcmd.py
@@ -1,4 +1,5 @@
 import selenium
+import glob
 import shlex
 import unittest
 import unittest
@@ -55,31 +56,32 @@ def wait_for_element_to_appear(func, output=False, **kwargs):
         if c > 25:
             raise Exception("Load Time out. Maybe your internet is slow or that contact doesnt exist.")
 
-def start_browser(browser,output=True, show_window=False):
+def start_browser(browser,output=True, show_window=False,firefox_profile=None):
+    # tries to find fire fox profile under its default linux user install path
+    firefox_profile_default = glob.glob('/home/{}/.mozilla/firefox/*.default'.format(os.environ['USER']))[0]
 
-    firefox_profile_default = '/home/{}/.mozilla/firefox/ax36q1xn.default'.format(os.environ['USER'])
+    if firefox_profile is None:
+        firefox_profile = firefox_profile_default
+
+    driver = None
 
     if browser == "firefox":
-        if output:
-            print("trying to connect to firefox browser")
+        out("trying to connect to firefox browser",output)
         fire_options = Options()
         if not show_window:
             fire_options.add_argument('--headless')
-        fp = webdriver.FirefoxProfile(firefox_profile_default)
+        fp = webdriver.FirefoxProfile(firefox_profile)
         driver = webdriver.Firefox(fp, options=fire_options)
-
-        redirect_to_whatapp(driver,output)
-
-        return driver
     elif browser == "chrome":
         driver = webdriver.ChromeOptions()
         options.add_argument("--user-data-dir={}".format(profile_path))
         options.add_argument("--no-startup-window") 
 
-        redirect_to_whatapp(driver,output)
-        return driver
     else:
         raise Exception("Unknown browser: {}".format(browser))
+
+    redirect_to_whatapp(driver,output)
+    return driver
 
 def send_message(message,reciever_name,browser,output=True,show_window=False, driver=None):
     """
@@ -101,6 +103,38 @@ def send_message(message,reciever_name,browser,output=True,show_window=False, dr
     text_area.send_keys(message)
     text_area.send_keys(Keys.ENTER)
 
+def list_all_contacts(driver=None,args=None):
+    # loads whatsapp web and then loads all the contact names
+    if driver is None:
+        driver = start_browser(args.browser,output,args.visible)
+        # wait for whatsapp to load by waiting for any title element to appear
+        
+        wait_for_element_to_appear(driver.find_element_by_xpath, xpath='//*[@title]')
+    elements = driver.find_elements_by_xpath('//*[@class="_19RFN _1ovWX _F7Vk" and @title]') # _19RFN _1ovWX _F7Vk
+    #loads whatsapp web and then loads all the contact names
+    for e in elements:
+        print(e.text)
+    # print(elements)
+
+def list_chat_messages(contact,driver=None,args=None):
+    element = None
+
+    if driver is None:
+        driver = start_browser(args.browser,args.output,args.show_window)
+        #wait for element to apear:
+        element = wait_for_element_to_appear(driver.find_element_by_xpath, xpath='//*[@title="{}"]'.format(name))
+    else:
+        element = driver.find_element_by_xpath('//*[@title="{}"]'.format(contact))
+
+    if element is None:
+        raise Exception("Contact '{}' not found".format(contact))
+    
+    element.click()
+    time.sleep(1)
+    elements = driver.find_elements_by_xpath('//*[@class="_F7Vk selectable-text invisible-space copyable-text"]')
+    for e in elements:
+        print(e.text) #.get_attribute('innerHTML')
+
 def parse_args(raw=None):
     # raw = raw_input('$: ') so same arguments can be used in the interactive shell
 
@@ -113,6 +147,7 @@ def parse_args(raw=None):
     parser.add_argument("-to", help="contact: by name")
 
     parser.add_argument("--browser", help="set browser firefox (default) / chrome", default="firefox")
+    parser.add_argument("--firefox-profile-path",help="to specify other user profile for firefox")
     parser.add_argument("--visible", help="should the botted browser be visible when run then add this option",nargs='?',type=int, const=1)
     parser.add_argument("--timeout", help="amount of time the bot should try to connect, default is 15 seconds",type=int, default=15)
 
@@ -120,10 +155,10 @@ def parse_args(raw=None):
         args = parser.parse_args()
     else:
         raw = shlex.split(raw)
-        print(raw)
+        out(raw,output)
         args = parser.parse_args(raw)
 
-    print(args)
+    out(args,output)
     return args
 
 def interactive_shell(args, output, test):
@@ -131,10 +166,13 @@ def interactive_shell(args, output, test):
     while True:
         astr = input('> ')
         if "exit" in astr:
-            out(output, "exiting shell and browser")
+            out("exiting shell and browser",output)
             break
         args = parse_args(raw=astr)
-        main(args, output, test, driver=driver)
+        try:
+            main(args, output, test, driver=driver)
+        except Exception as e:
+            print(str(e))
         print(args)
 
 def main(args, output, test, driver=None):
@@ -145,11 +183,13 @@ def main(args, output, test, driver=None):
         send_message(args.m,args.to,args.browser,show_window=args.visible,driver=driver)
     elif args.action == "list":
         # list available chat contacts
-        out(output, "Attemting to get contacts")
+        out("Attemting to get contacts",output)
+        list_all_contacts(driver,args=args)
     elif args.action == "view":
         # view chat
-        out(output, "attempting to load chat")
-    elif args.action == "shedule message":
+        out("attempting to load chat",output)
+        list_chat_messages(args.to,driver,args=args)
+    elif args.action == "schedule":
         test.assertIsNotNone(args.m, "please add a message to send: -m")
         test.assertIsNotNone(args.to, "please specify who to send the message to: -to ")
     elif args.action == "start":
